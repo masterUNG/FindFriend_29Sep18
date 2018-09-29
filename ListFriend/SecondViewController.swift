ac7138a7ff4523300313dab093a7847de5da8ad7
masterUNG <phrombutr@gmail.com>
2018-09-29 14:20:22 +0700

    Factor Number

--- ListFriend/SecondViewController.swift
+++ ListFriend/SecondViewController.swift
@@ -8,23 +8,60 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var numberFactorString: String = ""
+    var numberFactorInt: Int = 1
+    var numberMasterInt: Int = 1
+    var numberInts = [1]
+    
+    
+    
+    let crossString = " X "
+    
+    let numberLoopInt: Int = 12
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberLoopInt
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let showCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "NumberCell")
+        
+        while numberMasterInt < numberLoopInt {
+            
+            numberMasterInt += 1
+            numberInts.append(numberMasterInt)
+            
+        }
+        
+        showCell.textLabel?.text = String(numberInts[indexPath.row]) + crossString + String(numberFactorInt) + " = " + String(numberInts[indexPath.row] * numberFactorInt)
+        return showCell
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        numberFactorString = numberTextField.text!
+        print("numberFactor ==> \(numberFactorString)")
+        numberFactorInt = Int(numberFactorString)!
+        
+        
+        
+    }   // calcutateButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    }   // Main Function
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}   // Main Class
